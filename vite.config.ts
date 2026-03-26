e064d448a7a9c612d4a290beac16f0d0927bf4ed
ayoubc <ayoubch807@gmail.com>
2026-03-26 20:58:56 +0100

    fix: remove typeRoots

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/conway-game-of-life/',
+  build: {
+    outDir: './dist',
+    emptyOutDir: true, // also necessary
+  }
 })
